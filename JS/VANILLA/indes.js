cfdda4c6311899c3f91d298514b02d6b851a254e
Appe123123 <hjis0406@gmail.com>
2021-03-24 14:49:35 +0900

    JS

--- JS/VANILLA/indes.js
+++ JS/VANILLA/indes.js
@@ -1,1 +1,8 @@
-console.log('Im working. Its js')
+//JS의 선언은 let , const, (var)이 있음 
+//JS에서는 const를 주로 사용하는것을 권장 !
+
+
+const title = document.querySelector(".title");
+title.style.color = "red";
+title.innerHTML = "HI!";
+document.title = "i own you now";
