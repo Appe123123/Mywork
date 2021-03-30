5bd30415f3d4aa7263699573002169ce62e3f464
Appe123123 <hjis0406@gmail.com>
2021-03-30 17:56:22 +0900

    I'm Reacting

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+function Food({ favourite }) {
+    return <h1>I like {favourite}</h1>;
+}
+
 function App() {
- return <div className="App" />;
+    return (
+        <div>
+            <h1>test!</h1>
+            <Food favourite="kimchi" />
+            <Food favourite="ramen" />
+            <Food favourite="samgiopsal" />
+            <Food favourite="chukumi" />
+        </div>
+    );
 }
 
 export default App;
--- movieapp/src/index.js
+++ movieapp/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+//import exer from './exer';
 
 ReactDOM.render(<App />, document.getElementById('root') );
+
+//ReactDOM.render(<exer />, document.getElementById('root') );
 
